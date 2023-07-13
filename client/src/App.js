c9626184c614297e43fec4869b637b5e58013bc9
Luigi Molinengo <luigimolinengo@gmail.com>
2023-07-13 18:04:59 +0200

    FEAT: Started from Moyne logic, I've added my stile

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,49 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {TicketList} from "./components/TicketList";
 import UserContext from './context/UserContext';
 import Header from './components/Header';
-import { useState } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { HomepagePage,LoginPage, TicketPage } from './pages';
+import { useState, useEffect } from 'react';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { HomepagePage, LoginPage, TicketPage } from './pages';
 import TokenManager from './TokenManager';
-import {ProductTable} from "./components/ProductTable";
+import { ProductTable } from "./components/ProductTable";
+import Logout from "./components/Logout"
 
+function CheckHeader(props) {
+    const location = useLocation();
+
+    if (location.pathname.includes("/login")) {
+        return null; // Hide the menu on the login page
+    }
+
+    return <Header handleLogout={props.handleLogout} />;
+}
 
 function App() {
     const tokenManager = TokenManager();
-    const [user,setUser]=useState(tokenManager.retrieveUser());
-    const handleLogout = ()=>{
+    const [user, setUser] = useState(tokenManager.retrieveUser());
+    /*     const [loading, setLoading] = useState(true);
+    
+        useEffect(() => {
+            setLoading(false);
+        }, []); */
+
+    const handleLogout = () => {
         tokenManager.removeAuthToken();
         setUser(tokenManager.retrieveUser());
     }
+
     return (
-        <UserContext.Provider value={{user,setUser}}>
-            <Header handleLogout={handleLogout}/>
+        <UserContext.Provider value={{ user, setUser }}>
+            <CheckHeader handleLogout={handleLogout} />
             <Routes>
-                <Route index element={<Navigate replace to='/home'/>}/>
-                <Route path="/home" element={<HomepagePage/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/tickets" element={<TicketPage/>} />;
-                <Route path="/products" element={<ProductTable/>}/>;
+                <Route index element={<Navigate replace to='/home' />} />
+                <Route path="/home" element={<HomepagePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/tickets" element={<TicketPage />} />;
+                <Route path="/products" element={<ProductTable />} />;
+                <Route path="/logout" element={<Logout />} />;
             </Routes>
         </UserContext.Provider>
     );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,66 +1,98 @@
 import { useContext, useState } from "react";
-import { Form, Button, Alert } from "react-bootstrap";
+import { Row, Col, Form, Button, Container } from 'react-bootstrap';
+import ErrorMessage from '../components/ErrorMessage';
 import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Logo } from "../logo.svg";
+
 import UserContext from "../context/UserContext";
 import API from "../API";
-function LoginPage(props){
-    const navigate=useNavigate();
-    const {user,setUser}=useContext(UserContext);
-    const [role,setRole]=useState('');
-    const [username,setUsername]=useState('');
-    const [password,setPassword]=useState('');
-    const [error,setError]=useState(false);
-    const [errorMessage,setErrorMessage]=useState('');
-    return(
-        <Form style={
-            {
-                "margin": 0,
-                "position": "absolute",
-                "top": "50%",
-                "left": "50%",
-                "msTransform": "translate(-50%, -50%)",
-                "transform": "translate(-50%, -50%)"
+
+import "../styles/LoginPage.css"
+
+function LoginPage(props) {
+    const navigate = useNavigate();
+    const { user, setUser } = useContext(UserContext);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showError, setShowError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            console.log("Trying login");
+            await API.login(username, password, setUser);
+            navigate("/home");
+        }
+        catch (status) {
+            setShowError(true);
+            if (parseInt(status.message) === 500) {
+                setErrorMessage("Failed to contact the server.")
             }
-        } onSubmit={async e=>{
-            e.preventDefault();
-            e.stopPropagation();
-            try {
-                console.log("Trying login");
-                await API.login(username, password, setUser);
-                console.log("Chiamata API login eseguita");
-                console.log("Username:", username);
-                console.log("Password:", password);
-                navigate("/home");
+            else if (parseInt(status.message) === 401) {
+                setErrorMessage('Username or password is incorrect.');
             }
-            catch (status) {
-                setError(true);
-                if (parseInt(status.message) === 500) {
-                    setErrorMessage("Failed to contact the server.")
-                }
-                else if (parseInt(status.message) === 401) {
-                    setErrorMessage('Username or password is incorrect.');
-                }
-            }
-        }}>
-            <Form.Group className="mb-3">
-                <Form.Label style={{fontWeight:"bolder"}}>Username : </Form.Label>
-                <Form.Control type="text" placeholder="insert username" name="username" required value={username} onChange={e=>setUsername(e.target.value)} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label style={{fontWeight:"bolder"}}>Password : </Form.Label>
-                <Form.Control type="password" placeholder="insert password" name="password" required onChange={p=>setPassword(p.target.value)}/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Select style={{fontWeight:"bolder"}} value={role} required onChange={r=>setRole(r.target.value)}>
-                    <option value=''>Select your role!</option>
-                    <option value="customer">Customer</option>
-                    <option value="expert">Expert</option>
-                    <option value="manager">Manager</option>
-                </Form.Select>
-            </Form.Group>
-            <Button variant="success" type="submit">Submit</Button>
-            {error?<Alert className="my-3" variant="danger">{errorMessage}</Alert>:<></>}
-        </Form>
+        }
+    }
+
+    const handleErrorClose = () => {
+        setShowError(false);
+    };
+
+    return (
+
+        <div className='login-page-container d-flex align-items-center'>
+            <Container>
+                <Row>
+                    <Col md={6} xs={12} className="text-center">
+                        <Logo
+                            alt=""
+                            width="130"
+                            height="150"
+                            className="d-inline-block align-top"
+                        />
+                        <h1 className='text-login login-col'>MyTicketManager</h1>
+                        <br />
+                    </Col>
+                    <Col md={6} xs={12}>
+                        <Form>
+                            <Form.Group controlId="formUsername">
+                                <Form.Label className='text-login'>Username</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Enter username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </Form.Group>
+                            <br />
+                            <Form.Group controlId="formPassword">
+                                <Form.Label className='text-login'>Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Enter password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </Form.Group>
+                            <br />
+                            <div className='text-center btn-login'>
+                                <Button variant="primary" type="button" onClick={handleLogin}>
+                                    Login
+                                </Button>
+                                <Button variant="info" type="button">
+                                    Signup
+                                </Button>
+                                <Button variant="secondary" type="button">
+                                    Password forgotten?
+                                </Button>
+                            </div>
+                            <br />
+                            {showError && <ErrorMessage message={errorMessage} onClose={handleErrorClose} />}
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
