aa698602c2cac1b9bf06d19cc27a072be8152842
massipellegrino <max99.pellegrino@gmail.com>
2023-07-22 13:18:32 +0200

    fix: error message when no expert is found in ManageTicketForm

--- client/src/components/ManageTicketForm.js
+++ client/src/components/ManageTicketForm.js
@@ -15,13 +15,23 @@ function ManageTicketForm() {
     const [selectedExpert, setSelectedExpert] = useState(null)
 
     const [openAlert, setOpenAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+
+    const [expertsNotFound, setExpertsNotFound] = useState(false);
 
     async function load(){
         await API.getTicket(ticketId).then(res => {
             setTicket(res);
             if(res.expert) setSelectedExpert(res.expert.id);
             if(res.priorityLevel || res.priorityLevel === 0) setSelectedPriorityLevel(res.priorityLevel);
-            API.getExpertsBySector(res.product.sector.name).then(res => setSectorExperts(res))
+            API.getExpertsBySector(res.product.sector.name)
+                .then(res => setSectorExperts(res))
+                .catch(err => {
+                    setExpertsNotFound(true);
+                    setOpenAlert(true);
+                    setAlertMessage(`No experts found of the sector: ${res.product.sector.name}`);
+                    console.log(`No experts found of the sector: ${res.product.sector.name}`)
+                });
         });
     }
 
@@ -56,6 +66,9 @@ function ManageTicketForm() {
         }else{
             event.preventDefault();
             setOpenAlert(true);
+            setAlertMessage('Select both an expert and a priority level to save the ticket, ' +
+                'otherwise just click \'Cancel\' to go back to the ticket list ' +
+                'without changing anything');
         }
     }
 
@@ -75,107 +88,99 @@ function ManageTicketForm() {
         return `${year}-${month}-${day}, ${hours}:${minutes}`;
     }
 
-    if(!ticket || !sectorExperts)
-        return <div>Loading...</div>
-
-    return (
-        <>
-            <div className="manage-ticket-form">
-                <h4 className='text-center' style={{ marginBottom: '20px' }}>Manage the ticket with id '{ticketId}'</h4>
-                <span>{' '}</span>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Creation Date</th>
-                            <td>{reformatDate(ticket.creationDate)}</td>
-                        </tr>
-                        <tr>
-                            <th>Status</th>
-                            <td>{removeUnderscoresAndCapitalize(ticket.status)}</td>
-                        </tr>
-                        <tr>
-                            <th>Customer Email</th>
-                            <td>{ticket.profile.email}</td>
-                        </tr>
-                        <tr>
-                            <th>Customer Name</th>
-                            <td>{ticket.profile.name}</td>
-                        </tr>
-                        <tr>
-                            <th>Customer Surname</th>
-                            <td>{ticket.profile.surname}</td>
-                        </tr>
-                        <tr>
-                            <th>Customer Username</th>
-                            <td>{ticket.profile.username}</td>
-                        </tr>
-                        <tr>
-                            <th>Product Ean</th>
-                            <td>{ticket.product.ean}</td>
-                        </tr>
-                        <tr>
-                            <th>Product Brand</th>
-                            <td>{ticket.product.brand}</td>
-                        </tr>
-                        <tr>
-                            <th>Product Name</th>
-                            <td>{ticket.product.name}</td>
-                        </tr>
-                        <tr>
-                            <th>Product Sector</th>
-                            <td>{removeUnderscoresAndCapitalize(ticket.product.sector.name)}</td>
-                        </tr>
-                        <tr>
-                            <th>Expert (only experts of the related sector can be assigned to the ticket):</th>
-                            <td><Form.Select value = {selectedExpert ? selectedExpert.toString(): "null"} onChange={handleExpertChange}>
-                                <option value="null"></option>
-                                {sectorExperts.map(exp => {
-                                    return <option key={exp.id} value={exp.id.toString()}>{`${exp.name} ${exp.surname} [${exp.email} - ${exp.username}]`}</option>
-                                })}
-                            </Form.Select></td>
-                        </tr>
-                        <tr>
-                            <th>Ticket Priority Level</th>
-                            <td><Form.Select value = {(selectedPriorityLevel || selectedPriorityLevel === 0) ? selectedPriorityLevel.toString(): "null"} onChange={handlePriorityChange}>
-                                <option value="null"></option>
-                                <option value="0">0</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                            </Form.Select></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td>
-                                <Link reloadDocument to={'/tickets'} onClick={handleSave}>
-                                     <Button>Save</Button>
-                                </Link>
-                                <span>{' '}</span>
-                                <Link to={'/tickets'}>
-                                     <Button>Cancel</Button>
-                                </Link>
-                            </td>
-                        </tr>
-                    </thead>
-                </Table>
-            </div>
-            <Alert show={openAlert} variant="warning">
-                <Alert.Heading>Warning</Alert.Heading>
-                <p>
-                    Select both an expert and a priority level to save the ticket,
-                    otherwise just click 'Cancel' to go back to the ticket list
-                    without changing anything
-                </p>
-                <hr />
-                <div className="d-flex justify-content-end">
-                    <Button onClick={() => setOpenAlert(false)} variant="outline-warning">
-                        Close
-                    </Button>
+    if((!ticket || !sectorExperts) && !expertsNotFound)
+        return <div>Loading...</div>;
+    else
+        return (
+            <>
+                <div className="manage-ticket-form">
+                    <h4 className='text-center' style={{ marginBottom: '20px' }}>Manage the ticket with id '{ticketId}'</h4>
+                    <span>{' '}</span>
+                    <Alert show={openAlert} variant="warning" onClose={() => setOpenAlert(false)} dismissible>
+                        <Alert.Heading>Warning</Alert.Heading>
+                        <p>{alertMessage}</p>
+                    </Alert>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Creation Date</th>
+                                <td>{reformatDate(ticket.creationDate)}</td>
+                            </tr>
+                            <tr>
+                                <th>Status</th>
+                                <td>{removeUnderscoresAndCapitalize(ticket.status)}</td>
+                            </tr>
+                            <tr>
+                                <th>Customer Email</th>
+                                <td>{ticket.profile.email}</td>
+                            </tr>
+                            <tr>
+                                <th>Customer Name</th>
+                                <td>{ticket.profile.name}</td>
+                            </tr>
+                            <tr>
+                                <th>Customer Surname</th>
+                                <td>{ticket.profile.surname}</td>
+                            </tr>
+                            <tr>
+                                <th>Customer Username</th>
+                                <td>{ticket.profile.username}</td>
+                            </tr>
+                            <tr>
+                                <th>Product Ean</th>
+                                <td>{ticket.product.ean}</td>
+                            </tr>
+                            <tr>
+                                <th>Product Brand</th>
+                                <td>{ticket.product.brand}</td>
+                            </tr>
+                            <tr>
+                                <th>Product Name</th>
+                                <td>{ticket.product.name}</td>
+                            </tr>
+                            <tr>
+                                <th>Product Sector</th>
+                                <td>{removeUnderscoresAndCapitalize(ticket.product.sector.name)}</td>
+                            </tr>
+                            <tr>
+                                <th>Expert (only experts of the related sector can be assigned to the ticket):</th>
+                                <td><Form.Select value = {selectedExpert ? selectedExpert.toString(): "null"} onChange={handleExpertChange}>
+                                    <option value="null"></option>
+                                    {sectorExperts ?
+                                        sectorExperts.map(exp => {
+                                            return <option key={exp.id} value={exp.id.toString()}>{`${exp.name} ${exp.surname} [${exp.email} - ${exp.username}]`}</option>
+                                        }) : null
+                                    }
+                                </Form.Select></td>
+                            </tr>
+                            <tr>
+                                <th>Ticket Priority Level</th>
+                                <td><Form.Select value = {(selectedPriorityLevel || selectedPriorityLevel === 0) ? selectedPriorityLevel.toString(): "null"} onChange={handlePriorityChange}>
+                                    <option value="null"></option>
+                                    <option value="0">0</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </Form.Select></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <Link reloadDocument to={'/tickets'} onClick={handleSave}>
+                                         <Button>Save</Button>
+                                    </Link>
+                                    <span>{' '}</span>
+                                    <Link to={'/tickets'}>
+                                         <Button>Cancel</Button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        </thead>
+                    </Table>
                 </div>
-            </Alert>
-        </>
-    );
+            </>
+        );
 }
 
 export {ManageTicketForm}
